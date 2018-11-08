1abdbcca8102d8b0ba0b9a561e87f71fd54ba9d4
김주홎 <gyong0812@naver.com>
2018-11-08 15:50:03 +0900

    pointer 3 추가설명

--- main.c
+++ main.c
@@ -6,13 +6,19 @@
 int main(int argc, char *argv[]) {
 	
 	int x=10;
+	int y=20;
 	int *ptr;
 	
 	ptr=&x;
 	printf("ptr value :%d\n",*ptr);
 	
+	ptr=&y;
+	printf("ptr value :%d\n",*ptr);
+	
+	ptr=&x; //*ptr =&x -> 포인터값의 주소를 나타내는 것 
+	
 	*ptr=20;
-	printf("x value : %d",x);
+	printf("x value : %d\n",x);
 	
 	return 0;
 }
